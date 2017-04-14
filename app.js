301b97a5cb7af213422bd8b4ca12f480d3f5976a
codinghobby@gmail.com <codinghobby@gmail.com>
2017-04-14 10:11:24 +0200

    Added basic shapes

--- app.js
+++ app.js
@@ -2,12 +2,5 @@ const { Canvas } = require('./src/mare')
 
 window.addEventListener('load', function () {
 	let canvas = new Canvas(500, 500)
-	canvas.background(51)
-
-	canvas.shape([
-		[20, 20],
-		[120, 20],
-		[120, 120],
-		[20, 120]
-	])
+	canvas.ellipse()
 })
--- src/canvas.js
+++ src/canvas.js
@@ -35,6 +35,67 @@ module.exports = class Canvas {
 		}
 	}
 
+
+	/**
+	 * Change the canvas's fill color
+	 * 
+	 * @param {Number|Array} r - r parameter in rgb / color as rgb array ([r, g, b])
+	 * @param {Number} [g=r] - g paramter in rgb
+	 * @param {Number} [b=g] - b parameter in rgb
+	 * 
+	 * @return {Array[]} - the new fill color expressed as an rgb array ([r, ,g b])
+	 */
+	fill(r, g, b) {
+		// This we need to avoid strange type conversions (i.e. 0 defaulting to r)
+		// If g & b are undefined, it means the the color is being passed in as a grayscale value, so we want the 3 params to be equal
+		let rp = r
+		let gp = g != undefined ? g : rp
+		let bp = b != undefined ? b : gp
+
+		// Change fill color
+		if (typeof r == "number") {
+			this.ctx.fillStyle = `rgb(${rp}, ${gp}, ${bp})`
+			this.fillColor = [rp, gp, bp]
+		} else if (r instanceof Array) {
+			this.ctx.fillStyle = `rgb(${rp[0]}, ${rp[1]}, ${rp[2]}`
+			this.fillColor = [rp[0], rp[1], rp[2]]
+		} else {
+			throw new Error('Unsupported color format')
+		}
+		// return new fill color
+		return this.fillColor
+	}
+
+	/**
+	 * Change the canvas's stroke color
+	 * 
+	 * @param {Number|Array} r - r parameter in rgb / color as rgb array ([r, g, b])
+	 * @param {Number} [g=r] - g paramter in rgb
+	 * @param {Number} [b=g] - b parameter in rgb
+	 * 
+	 * @return {Array[]} - the new stroke color expressed as an rgb array ([r, ,g b])
+	 */
+	stroke(r, g, b) {
+		// This we need to avoid strange type conversions (i.e. 0 defaulting to r)
+		// If g & b are undefined, it means the the color is being passed in as a grayscale value, so we want the 3 params to be equal
+		let rp = r
+		let gp = g != undefined ? g : rp
+		let bp = b != undefined ? b : gp
+
+		// Change stroke color
+		if (typeof r == "number") {
+			this.ctx.strokeStyle = `rgb(${rp}, ${gp}, ${bp})`
+			this.strokeColor = [rp, gp, bp]
+		} else if (r instanceof Array) {
+			this.ctx.strokeStyle = `rgb(${rp[0]}, ${rp[1]}, ${rp[2]}`
+			this.strokeColor = [rp[0], rp[1], rp[2]]
+		} else {
+			throw new Error('Unsupported color format')
+		}
+		// return new stroke color
+		return this.strokeColor
+	}
+
 	/**
 	 * sets the element's background and updates class variables
 	 * 
@@ -53,15 +114,17 @@ module.exports = class Canvas {
 
 		// Change background
 		if (typeof r == "number") {
-			this.el.style.background = `rgb(${rp}, ${gp}, ${bp})`
+			this.ctx.fillStyle = `rgb(${rp}, ${gp}, ${bp})`
+			this.ctx.fillRect(0, 0, this.width, this.height)
 			this.bg = [rp, gp, bp]
 		} else if (r instanceof Array) {
-			this.el.style.background = `rgb(${rp[0]}, ${rp[1]}, ${rp[2]}`
+			this.ctx.fillStyle = `rgb(${rp[0]}, ${rp[1]}, ${rp[2]}`
+			this.ctx.fillRect(0, 0, this.width, this.height)
 			this.bg = [rp[0], rp[1], rp[2]]
 		} else {
 			throw new Error('Unsupported color format')
 		}
-		// Set this.background to the newly set background
+		// Return the new background color
 		return this.bg
 	}
 
@@ -86,6 +149,50 @@ module.exports = class Canvas {
 	}
 
 	/**
+	 * Draws a triangle on the canvas
+	 * 
+	 * @param {Number} [x=canvasWidth/2] - x position of the first vertex
+	 * @param {Number} [y=canvasHeight/2] - y position of the first vertex
+	 * @param {Number} [w=100] - width of the triangle
+	 * @param {Number} [h=w] - height of the triangle
+	 */
+	triangle(x, y, w, h) {
+		let xpos = x !== undefined ? x : this.width / 2
+		let ypos = y !== undefined ? y : this.height / 2
+		let width = w !== undefined ? w : 100
+		let height = h !== undefined ? h : width
+
+		this.ctx.fillStyle = '#FFFFFF'
+		this.ctx.beginPath()
+		this.ctx.moveTo(xpos, ypos)
+		this.ctx.lineTo((xpos + width) / 2, ypos + height)
+		this.ctx.lineTo(xpos + width, ypos)
+		this.ctx.closePath()
+		this.ctx.fill()
+	}
+
+
+	/**
+	 * Draws an ellipse on the screen
+	 * 
+	 * @param {Number} [x=canvasWIdth/2] - x position of the center
+	 * @param {Number} [y=canvasHeight/2] - y position of the center
+	 * @param {Number} [rx=100] - x radius
+	 * @param {Number} [ry=rx] - y radius
+	 */
+	ellipse(x, y, rx, ry) {
+		let xpos = x !== undefined ? x : this.width / 2
+		let ypos = y !== undefined ? y : this.height / 2
+		let radiusX = rx !== undefined ? rx : 100
+		let radiusY = ry !== undefined ? ry : radiusX
+
+		this.ctx.fillStyle = '#FFFFFF'
+		this.ctx.beginPath()
+		this.ctx.ellipse(xpos, ypos, radiusX, radiusY, 0, 0, 2 * Math.PI, false)
+		this.ctx.fill()
+	}
+
+	/**
 	 * Creates a path on the canvas based on an array of points
 	 * 
 	 * @param {Array[]} points - an array of order pairs representing points on a plane
@@ -95,12 +202,13 @@ module.exports = class Canvas {
 		// Verify that the arg is a valid point array
 		if (points && points.length >= 3) {
 			this.ctx.beginPath()
-			if(points[0][0] && points[0][1]) this.ctx.moveTo(points[0][0], points[0][1])
+			if (points[0][0] && points[0][1]) this.ctx.moveTo(points[0][0], points[0][1])
 			else throw new Error('Invalid starting point')
-			for(var i = 1; i < points.length; i++) {
-				if(points[i][0] && points[i][1]) this.ctx.lineTo(points[i][0], points[i][1])
+			for (var i = 1; i < points.length; i++) {
+				if (points[i][0] && points[i][1]) this.ctx.lineTo(points[i][0], points[i][1])
 				else throw new Error('Invalid point')
 			}
+			this.ctx.closePath()
 			this.ctx.fillStyle = "#FFFFFF"
 			this.ctx.fill()
 		}
